f92ca333c0f113f0a280c38be6d7b54c058da9fb
Yash Gupta <45476875+yashgupta-7@users.noreply.github.com>
2020-03-06 20:19:48 +0530

    Add files via upload

---
+++ task/nn_utils.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import numpy as np
+import datetime
+import time
+import pandas as pd
+
+
+class AE_3D_200(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_3D_200, self).__init__()
+        self.en1 = nn.Linear(n_features, 200)
+        self.en2 = nn.Linear(200, 100)
+        self.en3 = nn.Linear(100, 50)
+        self.en4 = nn.Linear(50, 3)
+        self.de1 = nn.Linear(3, 50)
+        self.de2 = nn.Linear(50, 100)
+        self.de3 = nn.Linear(100, 200)
+        self.de4 = nn.Linear(200, n_features)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):
+        return self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x)))))))
+
+    def decode(self, x):
+        return self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(self.tanh(x))))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+
+    def describe(self):
+        return 'in-200-100-50-3-50-100-200-out'
+
+
+class AE_big(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_big, self).__init__()
+        self.en1 = nn.Linear(n_features, 8)
+        self.en2 = nn.Linear(8, 6)
+        self.en3 = nn.Linear(6, 4)
+        self.en4 = nn.Linear(4, 3)
+        self.de1 = nn.Linear(3, 4)
+        self.de2 = nn.Linear(4, 6)
+        self.de3 = nn.Linear(6, 8)
+        self.de4 = nn.Linear(8, n_features)
+        self.tanh = nn.Tanh()
+
+    def encode(self, x):
+        return self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x)))))))
+
+    def decode(self, x):
+        return self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(self.tanh(x))))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+
+    def describe(self):
+        return 'in-8-6-4-3-4-6-8-out'
+
+
+def get_data(train_ds, valid_ds, bs):
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True, pin_memory=True),
+        DataLoader(valid_ds, batch_size=bs * 2, pin_memory=True),
+    )
---
+++ task/utils.py
@@ -0,0 +1,44 @@
+import os.path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import pickle
+
+import torch
+import torch.nn as nn
+# import torch.optim as optim
+import torch.utils.data
+
+from torch.utils.data import TensorDataset
+
+import my_matplotlib_style as ms
+
+
+from fastai import basic_data, basic_train
+from fastai import train as tr
+
+from nn_utils import get_data
+
+def std_error(x, axis=None, ddof=0):
+    return np.nanstd(x, axis=axis, ddof=ddof) / np.sqrt(2 * len(x))
+
+def plot_activations(learn, figsize=(12, 9), lines=['-', ':'], save=None, linewd=1, fontsz=14):
+    plt.figure(figsize=figsize)
+    for i in range(learn.activation_stats.stats.shape[1]):
+        thiscol = ms.colorprog(i, learn.activation_stats.stats.shape[1])
+        plt.plot(learn.activation_stats.stats[0][i], linewidth=linewd, color=thiscol, label=str(learn.activation_stats.modules[i]).split(',')[0], linestyle=lines[i % len(lines)])
+    plt.title('Weight means')
+    plt.legend(fontsize=fontsz)
+    plt.xlabel('Mini-batch')
+    if save is not None:
+        plt.savefig(save + '_means')
+    plt.figure(figsize=(12, 9))
+    for i in range(learn.activation_stats.stats.shape[1]):
+        thiscol = ms.colorprog(i, learn.activation_stats.stats.shape[1])
+        plt.plot(learn.activation_stats.stats[1][i], linewidth=linewd, color=thiscol, label=str(learn.activation_stats.modules[i]).split(',')[0], linestyle=lines[i % len(lines)])
+    plt.title('Weight standard deviations')
+    plt.xlabel('Mini-batch')
+    plt.legend(fontsize=fontsz)
+    if save is not None:
+        plt.savefig(save + '_stds')
